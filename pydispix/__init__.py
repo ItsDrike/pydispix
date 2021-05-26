cd5bed3726de04918842d39303d340e532bb9b6c
ItsDrike <itsdrikeofficial@gmail.com>
2021-05-27 01:07:08 +0200

    Expose the multiplexed objects

--- pydispix/__init__.py
+++ pydispix/__init__.py
@@ -3,6 +3,7 @@ from pydispix.canvas import Canvas, Pixel  # noqa
 from pydispix.client import Client  # noqa
 from pydispix.color import Color, Colour, parse_color, parse_colour  # noqa
 from pydispix.log import setup_logging
+from pydispix.multiplex import MultiAutoDrawer, MultiClient  # noqa
 from pydispix.ratelimits import RateLimitedEndpoint, RateLimiter  # noqa
 
 setup_logging()
---
+++ pydispix/multiplex/__init__.py
@@ -0,0 +1,2 @@
+from pydispix.multiplex.multi_client import MultiClient  # noqa
+from pydispix.multiplex.multi_autodraw import MultiAutoDrawer  # noqa
