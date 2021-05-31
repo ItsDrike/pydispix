404e237b288ce1f176e6d8f809c9dac1a729edb0
ItsDrike <itsdrikeofficial@gmail.com>
2021-05-31 15:10:29 +0200

    Don't use blanket noqa

--- pydispix/__init__.py
+++ pydispix/__init__.py
@@ -1,10 +1,10 @@
-from pydispix import churches  # noqa
-from pydispix.autodraw import AutoDrawer  # noqa
-from pydispix.canvas import Canvas, Pixel  # noqa
-from pydispix.church import ChurchClient  # noqa
-from pydispix.client import Client  # noqa
-from pydispix.color import Color, Colour, parse_color, parse_colour  # noqa
+from pydispix import churches  # noqa: F401: F401
+from pydispix.autodraw import AutoDrawer  # noqa: F401
+from pydispix.canvas import Canvas, Pixel  # noqa: F401
+from pydispix.church import ChurchClient  # noqa: F401
+from pydispix.client import Client  # noqa: F401
+from pydispix.color import Color, Colour, parse_color, parse_colour  # noqa: F401
 from pydispix.log import setup_logging
-from pydispix.ratelimits import RateLimitedEndpoint, RateLimiter  # noqa
+from pydispix.ratelimits import RateLimitedEndpoint, RateLimiter  # noqa: F401
 
 setup_logging()
